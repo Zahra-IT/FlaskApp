72486481d36955dbe71dadaac30d14f2ab1f6ec5
Fatima Alqassab <falqassab07@gmail.com>
2025-02-11 12:08:37 +0300

    t3

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from falsk import falsk
-
-app =Flask(__name__)
-
+from flask import Flask
+app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Flask App Running Behind Nginx in Docker"
+return "
+Flask App Running Behind Nginx in Docker!"
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0'. port=5000)
+if __name__ == '__main__':
+app.run(host='0.0.0.0', port=5000)
